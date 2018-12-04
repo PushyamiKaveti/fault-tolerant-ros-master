26f9fb390acf32a51e3bc9eb4a2bb7283fbc7399
Pushyamik <kaveti.p@husky.neu.edu>
2018-12-03 21:06:31 -0500

    changes to environment setup

--- ros_env_setup.sh
+++ ros_env_setup.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 export TURTLEBOT_3D_SENSOR=kinect
 export TURTLEBOT3_MODEL=burger
-export ROS_ROOT=/home/pushyamik/ros_catkin_ws/install_isolated/share/ros
-export ROS_PACKAGE_PATH=/home/pushyamik/cs7610_ws/src:/home/pushyamik/ros_catkin_ws/src/ros_comm/rosbag:/home/pushyamik/ros_catkin_ws/install_isolated/share:/opt/ros/kinetic/share
+export ROS_ROOT=<your-folder-path>/install_isolated/share/ros
+export ROS_PACKAGE_PATH=<your-folder-path>/src/ros_comm/rosbag:<your-folder-path>/install_isolated/share
 
-export ROS_MASTER_URI=http://pushyamik-u:11311/
+export ROS_MASTER_URI=http://pushyamik-u:11311/ #replace with your hostname
 
 export ROS_VERSION=1
 
 
-export LD_LIBRARY_PATH=/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag/lib:/home/pushyamik/ros_catkin_ws/install_isolated/lib:/opt/ros/kinetic/lib:/home/pushyamik/Qt5.7.0/5.7/gcc_64/lib:/home/pushyamik/apps/spinnaker/lib
+export LD_LIBRARY_PATH=<your-folder-path>/devel_isolated/rosbag/lib:<your-folder-path>/install_isolated/lib:$LD_LIBRARY_PATH
 
-export PATH=/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag/bin:/home/pushyamik/ros_catkin_ws/install_isolated/bin:/opt/ros/kinetic/bin:/home/pushyamik/bin:/home/pushyamik/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export PATH=<your-folder-path>/devel_isolated/rosbag/bin:<your-folder-path>/install_isolated/bin:$PATH
 
 
-export ROSLISP_PACKAGE_DIRECTORIES=/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag/share/common-lisp
+export ROSLISP_PACKAGE_DIRECTORIES=<your-folder-path>/devel_isolated/rosbag/share/common-lisp
 
 export ROS_DISTRO=kinetic
 
-export PYTHONPATH=/home/pushyamik/cs7610_ws/devel/lib/python2.7/dist-packages:/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag/lib/python2.7/dist-packages:/home/pushyamik/ros_catkin_ws/install_isolated/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages:/home/pushyamik/models/research:/home/pushyamik/models/research/slim
+export PYTHONPATH=<your-folder-path>/devel_isolated/rosbag/lib/python2.7/dist-packages:<your-folder-path>/install_isolated/lib/python2.7/dist-packages
+export PKG_CONFIG_PATH=<your-folder-path>/devel_isolated/rosbag/lib/pkgconfig:<your-folder-path>/install_isolated/lib/pkgconfig
 
-export PKG_CONFIG_PATH=/home/pushyamik/cs7610_ws/devel/lib/pkgconfig:/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag/lib/pkgconfig:/home/pushyamik/ros_catkin_ws/install_isolated/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig
+export CMAKE_PREFIX_PATH=<your-folder-path>/devel_isolated/rosbag:<your-folder-path>/install_isolated
 
-export CMAKE_PREFIX_PATH=/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag:/home/pushyamik/ros_catkin_ws/install_isolated:/opt/ros/kinetic
-
-export ROS_ETC_DIR=/home/pushyamik/ros_catkin_ws/install_isolated/etc/ros
+export ROS_ETC_DIR=<your-folder-path>/install_isolated/etc/ros
 
