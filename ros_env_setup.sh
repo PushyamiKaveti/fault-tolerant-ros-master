498f2fcc571b7c50ddfec3fc84bb1fd4bb6f7baa
Pushyamik <kaveti.p@husky.neu.edu>
2018-12-03 20:28:52 -0500

    environment variables

---
+++ ros_env_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export TURTLEBOT_3D_SENSOR=kinect
+export TURTLEBOT3_MODEL=burger
+export ROS_ROOT=/home/pushyamik/ros_catkin_ws/install_isolated/share/ros
+export ROS_PACKAGE_PATH=/home/pushyamik/cs7610_ws/src:/home/pushyamik/ros_catkin_ws/src/ros_comm/rosbag:/home/pushyamik/ros_catkin_ws/install_isolated/share:/opt/ros/kinetic/share
+
+export ROS_MASTER_URI=http://pushyamik-u:11311/
+
+export ROS_VERSION=1
+
+
+export LD_LIBRARY_PATH=/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag/lib:/home/pushyamik/ros_catkin_ws/install_isolated/lib:/opt/ros/kinetic/lib:/home/pushyamik/Qt5.7.0/5.7/gcc_64/lib:/home/pushyamik/apps/spinnaker/lib
+
+export PATH=/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag/bin:/home/pushyamik/ros_catkin_ws/install_isolated/bin:/opt/ros/kinetic/bin:/home/pushyamik/bin:/home/pushyamik/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+
+export ROSLISP_PACKAGE_DIRECTORIES=/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag/share/common-lisp
+
+export ROS_DISTRO=kinetic
+
+export PYTHONPATH=/home/pushyamik/cs7610_ws/devel/lib/python2.7/dist-packages:/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag/lib/python2.7/dist-packages:/home/pushyamik/ros_catkin_ws/install_isolated/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages:/home/pushyamik/models/research:/home/pushyamik/models/research/slim
+
+export PKG_CONFIG_PATH=/home/pushyamik/cs7610_ws/devel/lib/pkgconfig:/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag/lib/pkgconfig:/home/pushyamik/ros_catkin_ws/install_isolated/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig
+
+export CMAKE_PREFIX_PATH=/home/pushyamik/ros_catkin_ws/devel_isolated/rosbag:/home/pushyamik/ros_catkin_ws/install_isolated:/opt/ros/kinetic
+
+export ROS_ETC_DIR=/home/pushyamik/ros_catkin_ws/install_isolated/etc/ros
+
